f6f7a1c13790c3a7660b5fe330dc142854a68cab
AfitovaViktoriya <afitovaviktoria@gmail.com>
2022-05-09 18:51:56 +0300

    feat: added searchbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
+import React, { useState } from 'react';
 import './App.css';
 import { iconMainSearch } from './assets/index';
+import { SearchBar } from './components/SearchBar/SearchBar';
 
 function App() {
+  const [userName, setUserName] = useState('');
+
   return (
     <div className="App">
+      <SearchBar setUserName={setUserName} />
       <div className='initial-section'>
         <div className='initial-container'>
           <img className='initial-icon' src={iconMainSearch} alt='search'/>
--- my-app/src/assets/index.js
+++ my-app/src/assets/index.js
@@ -1,5 +1,9 @@
 import iconMainSearch from './iconMainSearch.svg';
+import logoGitHub from './logoGitHub.svg';
+import iconSearch from './iconSearch.svg';
 
 export {
 	iconMainSearch,
+	logoGitHub,
+	iconSearch
 }
